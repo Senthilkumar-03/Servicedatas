8b74685153f28d16778dfd607688d591e69909de
Senthilkumar-03 <80519208+Senthilkumar-03@users.noreply.github.com>
2021-03-12 17:32:21 +0530

    Update Services.java

--- users/WEB-INF/classes/Services.java
+++ users/WEB-INF/classes/Services.java
@@ -7,23 +7,27 @@ import java.lang.*;
 
 public class Services extends HttpServlet
 {
-	public native ArrayList services();
+	public native ArrayList services();        // JNI method which returns arraylist from C++
 	static
 	{
 		System.load("C:\\Program Files\\Apache Software Foundation\\Tomcat 8.5\\webapps\\users\\WEB-INF\\lib\\service.dll");
 	}
+	
+	// doGet method is hitted by the request from the frontend and executes
+	
 	public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
 	{
-		PrintWriter out = res.getWriter();
+		PrintWriter out = res.getWriter();               
 		ArrayList<HashMap> al = new ArrayList<>();
 		Services s = new Services();
 		al = s.services();
 		int len = al.size();
 		JSONObject jo = new JSONObject();
-		jo.put("servicedata",al);
+		jo.put("servicedata",al);                 // Adding the arraylist as value with key "servicedata"
 		out.print(jo);
 		
 	 } 
 	
 }
+	
 	
