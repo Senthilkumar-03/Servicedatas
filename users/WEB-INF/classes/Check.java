e25d37dc0c7249cbad451835e7b8b5c7ffbab568
Senthilkumar-03 <80519208+Senthilkumar-03@users.noreply.github.com>
2021-03-12 17:38:32 +0530

    Update Check.java

--- users/WEB-INF/classes/Check.java
+++ users/WEB-INF/classes/Check.java
@@ -4,44 +4,41 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.json.*;
 import java.lang.*;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class Check extends HttpServlet
 {
-	static Logger logger = Logger.getLogger(Check.class.getName());
-	public native boolean checks(String serviceName);
-	public native boolean stops(String serviceName);
+	public native boolean checks(String serviceName);   // JNI method for to check and start the services.
+	public native boolean stops(String serviceName);    // JNI method for to check and stop the services.
 	static
 	{
 		System.load("C:\\Program Files\\Apache Software Foundation\\Tomcat 8.5\\webapps\\users\\WEB-INF\\lib\\Startingservices.dll");
 	}
+	
+	// doGet method is hitted by the request from the frontend and accessed.
+	
 	public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
 	{
 		PrintWriter out = res.getWriter();
-		String serviceName = req.getParameter("services");
-		String process = req.getParameter("process");
+		String serviceName = req.getParameter("services");    // request with servicename from frontend
+		String process = req.getParameter("process");         // request with whattype of process from the frontend
 		JSONObject jo = new JSONObject();
 		Check c = new Check();
-		System.out.println(serviceName);
-		System.out.println(process);
-		if(process.equals("start"))
+		//System.out.println(serviceName);
+		//System.out.println(process);
+		if(process.equals("start"))                        // if the process as start the function calls the checks method
 		{
 			if(c.checks(serviceName))
 			{
-				System.out.println("In Java Service started SuccessFully");
-				logger.log(Level.INFO, "In Java Service started SuccessFully");
+				//System.out.println("In Java Service started SuccessFully");
 				jo.put("result","Service started Successfully");
 			}
-			else
+			else                      
 			{
-				System.out.println("In Java Service not started SuccessFully");
-				logger.log(Level.INFO, "In Java Service not started SuccessFully");
+				//logger.log(Level.INFO, "In Java Service not started SuccessFully");
 				jo.put("result","Service not started Successfully");
 			}
 		}
-		else if(process.equals("stop"))
+		else if(process.equals("stop"))                   // else the process as stop the function calls the stopsmethod
 		{
 			if(c.stops(serviceName))
 			{
